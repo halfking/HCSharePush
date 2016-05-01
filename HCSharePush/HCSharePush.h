c7138e16e43ebb0695b9c0a98e49ee1434dd3a64
halfking <kimmy.huang@gmail.com>
2016-05-02 03:37:11 +0800

    提交公用的头文件

---
+++ HCSharePush/HCSharePush.h
@@ -0,0 +1,25 @@
+//
+//  HCSharePush.h
+//  HCSharePush
+//
+//  Created by HUANGXUTAO on 16/5/1.
+//  Copyright © 2016年 seenvoice.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HCSharePush.
+FOUNDATION_EXPORT double HCSharePushVersionNumber;
+
+//! Project version string for HCSharePush.
+FOUNDATION_EXPORT const unsigned char HCSharePushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HCSharePush/PublicHeader.h>
+
+
+#import <HCSharePush/CMD_LogEvent.h>
+#import <HCSharePush/UMShareObject.h>
+#import <HCSharePush/CMD_BindGTCID.h>
+#import <HCSharePush/TMManager.h>
+#import <HCSharePush/shareConfig.h>
+#import <HCSharePush/HCShareConfig.h>
